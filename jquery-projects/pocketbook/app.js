04cac15df8fca05cc751fa196ba64887e5189631
joeltonmatos <joeltonmatos@gmail.com>
2017-07-31 10:52:30 -0300

    added jquery projects folder and pocketbook project

---
+++ jquery-projects/pocketbook/app.js
@@ -0,0 +1,37 @@
+function inicializateErrorFields() {
+    $(".first-name-error").text('');
+}
+
+var main = function () {
+
+    $('form').submit(function () {
+        inicializateErrorFields();
+        
+        var firstName = $('#first').val();
+        var lastName = $('#last').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+
+        if (firstName === "") {
+            $(".first-name-error").text('Please, enter your first name.');
+        }
+
+        if (lastName === "")
+            $('.last-name-error').text("Please enter your last name.");
+
+        if (email === "")
+            $('.email-error').text("Please enter your email address");
+        else if (email === "joeltonmatos@gmail.com")
+            $('.email-error').text("This email is already taken.");
+
+        if (password === "")
+            $('.password-error').text("Please enter a password");
+        else if (password.length < 8)
+            $('.password-error').text("Short passwords are easy to guess. Try one with at least 8 characters.");
+
+
+        return false;
+    })
+}
+
+$(document).ready(main);
